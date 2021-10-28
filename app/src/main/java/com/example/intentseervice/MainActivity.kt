1b65309dfd7ba48f0554f6cd55adca8d2f7b1f96
Oluwafemijohn <femiogundipe01@gmail.com>
2021-10-28 08:23:29 +0100

    Service learnt

--- app/src/main/java/com/example/intentseervice/MainActivity.kt
+++ app/src/main/java/com/example/intentseervice/MainActivity.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var startButton: Button
     private lateinit var stopButton: Button
     private lateinit var textView: TextView
+    private lateinit var serviceTextView: TextView
+    private lateinit var editText: EditText
+    private lateinit var sendButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,16 +22,38 @@ class MainActivity : AppCompatActivity() {
         startButton =findViewById(R.id.start_button)
         stopButton =findViewById(R.id.stop_button)
         textView = findViewById(R.id.textview)
+        serviceTextView = findViewById(R.id.service_text_view)
+        editText = findViewById(R.id.editText)
+        sendButton = findViewById(R.id.send_data )
+
 
         startButton.setOnClickListener{
-            Intent(this, MyIntentService::class.java).also {
+//            Intent(this, MyIntentService::class.java).also {
+//                startService(it)
+//                textView.text = "Intent service running"
+//            }
+            Intent(this, MyService::class.java).also {
                 startService(it)
-                textView.text = "Service running"
+                serviceTextView.text = "Service running"
             }
         }
 
         stopButton.setOnClickListener {
-            MyIntentService.stopService()
+//            MyIntentService.stopService()
+//            textView.text = "Service stopped"
+
+            Intent(this, MyService::class.java).also {
+                startService(it)
+                serviceTextView.text = "Service stopped"
+            }
+        }
+
+        sendButton.setOnClickListener {
+            Intent(this, MyService::class.java).also {
+                val text = editText.text.toString()
+                it.putExtra("EXTRA_DATA", text)
+                startService(it)
+            }
         }
 
 
---
+++ app/src/main/java/com/example/intentseervice/MyService.kt
@@ -0,0 +1,40 @@
+package com.example.intentseervice
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class MyService: Service() {
+    override fun onBind(intent: Intent?): IBinder? = null
+    val TAG = "MyTag"
+    init {
+        Log.d(TAG, "Service is running :.....: ")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val dataString = intent?.getStringExtra("EXTRA_DATA")
+        dataString?.let {
+            Log.d(TAG, "onStartCommand: Service is running ")
+        }
+
+//        to create thread
+        Thread{
+            while(true) {
+
+            }
+        }.start()
+
+//        without creating a separate thread, the code below freezes app
+//        while(true) {
+//
+//        }
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: Service is been killed....")
+    }
+
+}
